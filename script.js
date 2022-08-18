596d608583cd4098aa66e5307d4f84bf25ece31c
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-08-18 19:22:36 +0500

    checking for form data

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ let tododata = [];
 // window.localStorage.setItem()
 // window.localStorage.getItem()
 
-const Form = document.getElementById("inputForm");
+let form = document.getElementById("inputForm");
+function inputHandeler (event) {
+  console.log(event.target);
+  event.target.preventDefault;
+  tododata.push(e.target.value);
+  alert("imp handler");
+console.log("1")
 
-const inputHandeler =   (event) => {
+};
 
-    console.log(event.target);
-    event.target.preventDefault;
-    tododata.push(e.target.value);
-    alert('imp handler');
+form.addEventListener("submit", inputHandeler);
+console.log("1")
 
 
-
-}
-Form.addEventListener("submit", inputHandeler);
-
-{   //appling some classes to li
-  let li_list = document.getElementsByTagName("li");
-  let looplen = li_list.length;
-  for (let i = 0; i < looplen; i++) {
-    li_list[i].classList.add("text-center", "p-2", "m-3");
-  }
-}
+// {
+//   //appling some classes to li
+//   let li_list = document.getElementsByTagName("li");
+//   let looplen = li_list.length;
+//   for (let i = 0; i < looplen; i++) {
+//     li_list[i].classList.add("text-center", "p-2", "m-3");
+//   }
+// }
