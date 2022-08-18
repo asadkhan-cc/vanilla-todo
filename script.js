6db0e19fefc973d501203a8dfa9c509d4be1faa2
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-08-18 21:36:01 +0500

    local storage module done

--- script.js
+++ script.js
@@ -1,27 +1,88 @@
 let tododata = [];
+const form = document.getElementById("form");
+form.addEventListener("submit", logSubmit);
 
-// window.localStorage.setItem()
-// window.localStorage.getItem()
+// =========================================
 
-let form = document.getElementById("inputForm");
-function inputHandeler (event) {
-  console.log(event.target);
-  event.target.preventDefault;
-  tododata.push(e.target.value);
-  alert("imp handler");
-console.log("1")
+window.onload = loadTasks();
 
+function loadTasks() {
+  // Get the tasks from localStorage on load
+  let tasks = storageGet()
+  console.log(tasks)
+   appenditems(tasks);
+  
+}
+// =========================================
+// localStorage.clear();
+
+
+function storageSet(arr_params) {
+  localStorage.clear();
+  // alert("storageSet start")
+  localStorage.setItem("tododata", JSON.stringify(arr_params));
+}
+function storageGet() {
+  // alert("storageGET start",localStorage.getItem("tododata"))
+  return JSON.parse(localStorage.getItem("tododata"));
+}
+
+function logSubmit(event) {
+  event.preventDefault();
+  tododata.push(event.target[0].value);
+  // storageSet(tododata);
+  event.target[0].value = "";
+  storageSet(tododata);
+  console.log(storageGet());
+}
+
+// printing user data
+
+//maping function
+
+
+// let tododata_storage = storageGet();
+
+function createNewTaskElement(taskString) {
+  // create List Item
+  let listItem = document.createElement("li");
+  // label
+  let label = document.createElement("label");
+  // input (text)
+  let editInput = document.createElement("input");
+  // button.edit
+  let editButton = document.createElement("button");
+  // button.delete
+  let deleteButton = document.createElement("button");
+
+  editInput.type = "text";
+  editButton.innerText = "Edit";
+  deleteButton.innerText = "Delete";
+  editInput.className = "hidden";
+  editButton.classList.add("text-center", "p-2", "m-3");
+  deleteButton.classList.add("text-center", "p-2", "m-3");
+
+  label.innerText = taskString;
+
+  // Each element needs appending
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
+  document.getElementById("ol_list").appendChild(listItem);
 };
+function appenditems(arr) {
+  arr.map((elem, index) => {
+    createNewTaskElement(elem);
+  });
+}
 
-form.addEventListener("submit", inputHandeler);
-console.log("1")
 
-
-// {
-//   //appling some classes to li
-//   let li_list = document.getElementsByTagName("li");
-//   let looplen = li_list.length;
-//   for (let i = 0; i < looplen; i++) {
-//     li_list[i].classList.add("text-center", "p-2", "m-3");
-//   }
-// }
+{
+  //appling some classes to li
+  let li_list = document.getElementsByTagName("li");
+  let looplen = li_list.length;
+  for (let i = 0; i < looplen; i++) {
+    li_list[i].classList.add("text-center", "p-10", "m-3");
+  }
+}
