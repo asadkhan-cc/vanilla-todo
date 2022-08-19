12137326ba7e090a3e542c0675d4cc9215c2c07f
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-08-19 22:32:02 +0500

    alhamdulillah finished but still have 2 known bugs

---
+++ Untitled-1.js
@@ -0,0 +1,123 @@
+{
+    "isTrusted": true
+}isTrusted: true
+altKey: false
+altitudeAngle: 1.5707963267948966
+azimuthAngle: 0
+bubbles: true
+button: 0
+buttons: 0
+cancelBubble: false
+cancelable: true
+clientX: 690
+clientY: 372
+composed: true
+ctrlKey: false
+currentTarget: null
+defaultPrevented: false
+detail: 1
+eventPhase: 0
+fromElement: null
+height: 1
+isPrimary: false
+layerX: 690
+layerY: 372
+metaKey: false
+movementX: 0
+movementY: 0
+offsetX: 404
+offsetY: 18
+pageX: 690
+pageY: 372
+path: Array(10)
+0: button.text-center.p-2.m-1.bg-red-300.rounded-full
+1: li#todo-1.text-center.p-1.m-3.flex.justify-end.alighn-center
+2: ol#ol_list
+3: div#output div.w-max.mt-10.m-auto
+4: div.w-max.mt-10.m-auto
+5: div.container
+6: body
+7: html
+8: document
+9: Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+length: 10
+[[Prototype]]: Array(0)
+pointerId: 1
+pointerType: "mouse"
+pressure: 0
+relatedTarget: null
+returnValue: true
+screenX: 690
+screenY: 472
+shiftKey: false
+sourceCapabilities: InputDeviceCapabilities {firesTouchEvents: false}
+srcElement: button.text-center.p-2.m-1.bg-red-300.rounded-full
+tangentialPressure: 0
+target: button.text-center.p-2.m-1.bg-red-300.rounded-full
+tiltX: 0
+tiltY: 0
+timeStamp: 2228.7999999998137
+toElement: null
+twist: 0
+type: "click"
+view: Window
+/template.html: "p-1 m-3 justify-end alighn-center grows self-center hidden ml-10 m-1 bg-blue-300 bg-red-300"
+alert: ƒ alert()
+appenditems: ƒ appenditems(arr)
+atob: ƒ atob()
+blur: ƒ blur()
+btoa: ƒ btoa()
+caches: CacheStorage {}
+cancelAnimationFrame: ƒ cancelAnimationFrame()
+cancelIdleCallback: ƒ cancelIdleCallback()
+captureEvents: ƒ captureEvents()
+chrome: {loadTimes: ƒ, csi: ƒ}
+clear: ƒ clear()
+clearInterval: ƒ clearInterval()
+clearTimeout: ƒ clearTimeout()
+clientInformation: Navigator {vendorSub: '', productSub: '20030107', vendor: 'Google Inc.', maxTouchPoints: 1, scheduling: Scheduling, …}
+close: ƒ close()
+closed: false
+confirm: ƒ confirm()
+cookieStore: CookieStore {onchange: null}
+createImageBitmap: ƒ createImageBitmap()
+createNewTaskElement: ƒ createNewTaskElement(taskString, id)
+crossOriginIsolated: false
+crypto: Crypto {subtle: SubtleCrypto}
+customElements: CustomElementRegistry {}
+defaultStatus: ""
+defaultstatus: ""
+deleting_Func: ƒ deleting_Func(e)
+devicePixelRatio: 2
+document: document
+editing_Func: ƒ editing_Func(e)
+external: External {}
+fetch: ƒ fetch()
+find: ƒ find()
+focus: ƒ focus()
+frameElement: null
+frames: Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+getComputedStyle: ƒ getComputedStyle()
+getScreenDetails: ƒ getScreenDetails()
+getSelection: ƒ getSelection()
+history: History {length: 1, scrollRestoration: 'auto', state: null}
+indexedDB: IDBFactory {}
+innerHeight: 513
+innerWidth: 550
+isSecureContext: true
+launchQueue: LaunchQueue {}
+length: 0
+listyles: ƒ listyles(param)
+loadTasks: ƒ loadTasks()
+localStorage: Storage {tododata: '["a","s","1","2","3","q","w","e"]', length: 1}
+location: Location {ancestorOrigins: DOMStringList, href: 'http://127.0.0.1:5500/', origin: 'http://127.0.0.1:5500', protocol: 'http:', host: '127.0.0.1:5500', …}
+locationbar: BarProp {visible: true}
+logSubmit: ƒ logSubmit(event)
+matchMedia: ƒ matchMedia()
+menubar: BarProp {visible: true}
+moveBy: ƒ moveBy()
+moveTo: ƒ moveTo()
+name: ""
+navigation: Navigation {currentEntry: NavigationHistoryEntry, transition: null, canGoBack: false, canGoForward: false, onnavigate: null, …}
+navigator: Navigator {vendorSub: '', productSub: '20030107', vendor: 'Google Inc.', maxTouchPoints: 1, scheduling: Scheduling, …}
+onabort: null
--- script.js
+++ script.js
@@ -7,43 +7,56 @@ form.addEventListener("submit", logSubmit);
 window.onload = loadTasks();
 
 function loadTasks() {
+  console.log("before load", tododata);
   // Get the tasks from localStorage on load
-  let tasks = storageGet()
-  console.log(tasks)
-   appenditems(tasks);
-  
+  let tasks = storageGet();
+  console.log("load task working", tasks);
+  appenditems(tasks);
+  console.log(tasks);
+  if (tasks !== null) {
+    tasks.forEach((element) => {
+      tododata.push(element);
+    });
+  }
+
+  console.log("before load", tododata);
 }
 // =========================================
-// localStorage.clear();
-
 
 function storageSet(arr_params) {
-  localStorage.clear();
-  // alert("storageSet start")
+  // localStorage.clear();
+  // alert("storageSet start");
+  console.log(JSON.stringify(arr_params))
   localStorage.setItem("tododata", JSON.stringify(arr_params));
 }
 function storageGet() {
-  // alert("storageGET start",localStorage.getItem("tododata"))
-  return JSON.parse(localStorage.getItem("tododata"));
+  // alert("storageGET start", JSON.stringify(localStorage.getItem("tododata")));
+  if (localStorage.getItem("tododata") == "undefined") {
+    return localStorage.getItem("tododata");
+  } else return JSON.parse(localStorage.getItem("tododata"));
+
+  // console.log()
 }
 
 function logSubmit(event) {
   event.preventDefault();
   tododata.push(event.target[0].value);
   // storageSet(tododata);
-  event.target[0].value = "";
+  console.log("before storage", tododata);
   storageSet(tododata);
-  console.log(storageGet());
+  console.log("after storage", tododata);
+
+  console.log("storage get", storageGet());
+  createNewTaskElement(event.target[0].value, storageGet().length);
+  event.target[0].value = "";
+  listyles();
 }
 
 // printing user data
 
-//maping function
-
-
 // let tododata_storage = storageGet();
 
-function createNewTaskElement(taskString) {
+function createNewTaskElement(taskString, id) {
   // create List Item
   let listItem = document.createElement("li");
   // label
@@ -52,37 +65,179 @@ function createNewTaskElement(taskString) {
   let editInput = document.createElement("input");
   // button.edit
   let editButton = document.createElement("button");
+  // button.editupdate
+  let updateEditButton = document.createElement("button");
   // button.delete
   let deleteButton = document.createElement("button");
 
+  listItem.id = `todo-${id}`;
+  label.classList.add("grows", "self-center");
   editInput.type = "text";
   editButton.innerText = "Edit";
   deleteButton.innerText = "Delete";
-  editInput.className = "hidden";
-  editButton.classList.add("text-center", "p-2", "m-3");
-  deleteButton.classList.add("text-center", "p-2", "m-3");
+  updateEditButton.innerText = "Update";
+  //======================================================
+  editInput.classList.add(
+    "hidden",
+    "inpBorder",
+    "rounded-full",
+    "pl-3",
+    "bg-gray-200","self-center"
+  );
 
+  updateEditButton.className = "hidden";
+
+  updateEditButton.classList.add(
+    "button2",
+    "text-center",
+    "p-1",
+    "px-2",
+    "ml-10",
+    "m-1",
+    "bg-green-300",
+    "rounded-full"
+  );
+  editButton.classList.add(
+    "button1",
+    "text-center",
+    "p-1",
+    "ml-10",
+    "m-1",
+    "bg-blue-300",
+    "rounded-full"
+  );
+  deleteButton.classList.add(
+    "button2",
+    "text-center",
+    "p-1",
+    "m-1",
+    "bg-red-300",
+    "rounded-full"
+  );
+
+  deleteButton.addEventListener("click", deleting_Func);
+  editButton.addEventListener("click", editing_Func);
+  updateEditButton.addEventListener("click", updating_func);
   label.innerText = taskString;
 
   // Each element needs appending
   listItem.appendChild(label);
   listItem.appendChild(editInput);
+  listItem.appendChild(updateEditButton);
   listItem.appendChild(editButton);
   listItem.appendChild(deleteButton);
   document.getElementById("ol_list").appendChild(listItem);
-};
+}
+//maping function
 function appenditems(arr) {
-  arr.map((elem, index) => {
-    createNewTaskElement(elem);
-  });
+  try {
+    if (typeof arr === "object") {
+      arr.map((elem, index) => {
+        createNewTaskElement(elem, index + 1);
+      });
+    } else createNewTaskElement(arr);
+  } catch (error) {
+    console.log("some initial error");
+  }
 }
 
-
-{
+function listyles(param) {
   //appling some classes to li
   let li_list = document.getElementsByTagName("li");
   let looplen = li_list.length;
   for (let i = 0; i < looplen; i++) {
-    li_list[i].classList.add("text-center", "p-10", "m-3");
+    li_list[i].classList.add(
+      "text-center",
+      "p-1",
+      "m-3",
+      "flex",
+      "justify-end",
+      "alighn-center"
+    );
   }
+}
+
+document.getElementById("clear").addEventListener("click", clear);
+function clear() {
+  localStorage.clear();
+  location.reload(true);
+}
+listyles();
+
+function editing_Func(e) {
+  // alert("edit clicked");
+  let elem_id = e.path[1].id;
+  let main_elem = document.getElementById(elem_id);
+  console.log(main_elem);
+  //making edit button invisible
+  main_elem.children[3].style.display = "none";
+
+  let label = main_elem.children[0];
+  label.style.display = "none";
+  let inp = main_elem.children[1];
+  inp.classList.remove("hidden");
+  let editUpdateButton = main_elem.children[2];
+  editUpdateButton.style.display = "block";
+  let elem_arr = storageGet();
+  inp.value = label.innerText;
+  console.log(inp, editUpdateButton.innerText);
+}
+
+function deleting_Func(e) {
+  // alert("delete clicked");
+  console.log(e.path[1].id);
+  let elem_id = e.path[1].id;
+  let main_elem = document.getElementById(elem_id);
+  main_elem.style.display = "none";
+  console.log(main_elem);
+  let child = main_elem.firstChild;
+
+  let elem_arr = storageGet();
+
+  console.log(child.innerText);
+  // const upd_arr = elem_arr.map((elem,index) => {
+  //   if (elem == child.innerText ){
+  //     elem_arr.splice(index, 1)
+  //   }
+  //  });
+
+  //=========this code deletes item from local storage
+  let del_elem_index = elem_arr.indexOf(child.innerText);
+  elem_arr.splice(del_elem_index, 1);
+  console.log("updated array", elem_arr);
+  //=========this code updates item from local storage
+  storageSet(elem_arr);
+}
+function updating_func(e) {
+  // alert("edit update clicked");
+  let elem_id = e.path[1].id;
+  let main_elem = document.getElementById(elem_id);
+  console.log(main_elem);
+  //making edit button visible again and update edit invisible
+  main_elem.children[3].style.display = "block";
+  main_elem.children[2].style.display = "none";
+
+  let label = main_elem.children[0];
+  label.style.display = "";
+  let inp = main_elem.children[1];
+  console.log(inp.value, "update inp data value");
+  inp.classList.add("hidden");
+  let elem_arr = storageGet();
+  console.log(elem_arr, "before change");
+  let isBroken = false;
+  elem_arr.map((elem, index) => {
+    if (isBroken) {
+      return;
+    }
+    if (elem === label.innerText) {
+      elem_arr[index] = inp.value;
+      isBroken = true;
+      return;
+    }
+  });
+  label.innerText = inp.value;
+
+  console.log(elem_arr, "after change");
+  storageSet(elem_arr);
+  console.log(storageGet(),"getting updated array from local storage");
 }
